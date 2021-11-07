83966f2eccf2331cc5ee1058dd42e12c98e46ab0
Hyo Hyung Lee <lhhyung91@gmail.com>
2021-11-07 11:08:43 +0900

    [build] css-reset, global-style 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import { palette } from "./styles/color";
 
 function App() {
-  return <div>대기 중</div>;
+  return (
+    <div>
+      <GlobalStyles />
+      <ThemeProvider theme={palette}>
+        <div>대기 중</div>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/styles/color.ts
@@ -0,0 +1,9 @@
+export const palette = {
+  blue: "#228be6",
+  black: "#333333",
+  pink: "#f06595",
+  white: "#ffffff",
+  gray: "#878787",
+  darkgray: "#626262",
+  red: "#ff0000",
+};
